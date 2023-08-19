b8cc7c0e9da69b1178e7e9a8c2af493c2df4be12
Guilherme marx <guilhermemarx14@gmail.com>
2023-08-19 03:29:04 -0300

    linter correction

--- 009-react-features/src/App.tsx
+++ 009-react-features/src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [username, setUsername] = useState('guilhermemarx');
+  const username = 'guilhermemarx';
 
   return <div>{username.length >= 5 && <h1>Hello {username}</h1>}</div>;
 }
